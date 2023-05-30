a58b666a7f4a3ab19c0a9ffd000d92b5608f5615
Clara Dahan <crdahan@gmail.com>
2023-05-30 15:59:28 +0100

    nest

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,24 @@
 class BookingsController < ApplicationController
-  def create
+  def new
+    @booking = Booking.new
+    @island = Island.find(params[:island_id])
+  end
 
+  def create
+    @booking = Booking.new(booking_params)
+    @island = Island.find(params[:island_id])
+    @booking.island = @island
+    @booking.save
+    redirect_to island_path(@island)
   end
 
   def show
     @booking = Booking.find(params[:id])
   end
 
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :confirmed)
+  end
 end
