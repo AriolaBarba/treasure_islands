246a48475ab7d2feed3563de678654c70426e98f
Olly Crole <oliver.crole@gmail.com>
2023-05-30 16:06:06 +0100

    comple nuuu

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -14,7 +14,7 @@ class IslandsController < ApplicationController
   def create
     @island = Island.new(island_params)
     @island.save
-    redirect_to islands_path(@island)
+    redirect_to island_path(@island)
   end
 
   private
