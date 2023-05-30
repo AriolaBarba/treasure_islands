9cada31a480fdd941d391a743c5bbb07330fb543
Ariola Barbatovci <a.barbatovci92@gmail.com>
2023-05-30 17:40:04 +0100

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "islands#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/mybookings', to: 'bookings#mybookings'
+
   resources :islands do
     resources :bookings
   end
