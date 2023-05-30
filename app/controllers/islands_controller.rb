fb5b2abecdfde6b8f89d3f0cd4c440d0b0f2c9a8
Clara Dahan <crdahan@gmail.com>
2023-05-30 14:05:05 +0100

    added create

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -8,7 +8,14 @@ class IslandsController < ApplicationController
   end
 
   def create
+    @island = Island.new(island_params)
+    @island.save
+  end
 
+  private
+
+  def island_params
+  params.require(:island).permit(:name, :description, :location, :price)
   end
 
 end
